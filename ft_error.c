8780128467695cf2bebcbb1ecbfec18a2fb77ae2
jgrimaud <jgrimaud@student.42lyon.fr>
2024-05-29 11:41:18 +0200

    cleanup and fix exit, export, combine_str,

--- ft_error.c
+++ ft_error.c
@@ -6,13 +6,29 @@
 /*   By: jgrimaud <jgrimaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 22:25:07 by jgrimaud          #+#    #+#             */
-/*   Updated: 2024/03/14 00:40:15 by jgrimaud         ###   ########.fr       */
+/*   Updated: 2024/05/29 11:18:05 by jgrimaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-ssize_t	write_error(char *msg)
+/**
+ * Write an error message to stderr. Must end with NULL.
+ * @param dummy Unused.
+ * @param ... The error messages.
+ */
+void	write_error(int dummy, ...)
 {
-	return (write(2, msg, ft_strlen(msg)));
+	va_list	args;
+	char	*msg;
+
+	write(2, "minishell: ", 11);
+	va_start(args, dummy);
+	msg = va_arg(args, char *);
+	while (msg)
+	{
+		write(2, msg, ft_strlen(msg));
+		msg = va_arg(args, char *);
+	}
+	va_end(args);
 }
--- ft_exit.c
+++ ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: jgrimaud <jgrimaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 22:09:54 by jgrimaud          #+#    #+#             */
-/*   Updated: 2024/04/08 19:23:49 by jgrimaud         ###   ########.fr       */
+/*   Updated: 2024/05/29 11:06:40 by jgrimaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	clean_exit(int status, void *data)
 
 void	clean_exit_with_error(char *msg, void *data)
 {
-	write_error("Error: ");
-	write_error(msg);
-	write_error("\n");
+	write_error(0, "Error: ", msg, "\n", NULL);
 	clean_exit(1, data);
 }
